1c40098aab92fae2e5a770479fd6faec5b3dac30
RoandyellDagoy <roandyellkhendagoy@gmail.com>
2025-09-18 15:29:51 +0800

    50% completion

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext  } from "react";
 import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 interface Task {
   id: number;
@@ -7,14 +9,22 @@ interface Task {
   description: string;
 }
 
+  
+   
 const Dashboard: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [editId, setEditId] = useState<number | null>(null);
+ 
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
 
- 
- 
+  const handleLogout = () => {
+    auth?.logout(); 
+    navigate("/"); 
+   }
+
     const fetchTasks = async () => {
       try {
         const res = await api.get('/tasks');
@@ -102,7 +112,8 @@ useEffect(() => {
   };
 
   return (
-    <div className="p-6 max-w-2xl mx-auto">
+    <div className="flex p-6 max-w-2xl mx-auto gap-10">
+      <div>
       <h1 className="text-2xl font-bold mb-4">Dashboard - Tasks</h1>
 
       {/* Task Form */}
@@ -125,40 +136,50 @@ useEffect(() => {
           {editId ? "Update Task" : "Add Task"}
         </button>
       </form>
+      </div>
 
+
+    <div className="card bg-base-100card bg-base-100 w-96 drop-shadow-2xl">
       {/* Task List */}
      <ul className="space-y-3">
-  {Array.isArray(tasks) && tasks.length > 0 ? (
-    tasks.map((task, index) => (
-      <li
-        key={`${task.id}-${index}`}
-        className="card shadow p-4 flex justify-between items-center"
-      >
-        <div>
-          <h2 className="font-semibold">{task.title}</h2>
-          <p className="text-gray-600">{task.description}</p>
-        </div>
+          {Array.isArray(tasks) && tasks.length > 0 ? (
+            tasks.map((task, index) => (
+                <li
+                  key={`${task.id}-${index}`}
+                  className="card shadow p-4 flex justify-between items-center"
+                >
+                  <div>
+                    <h2 className="font-semibold">{task.title}</h2>
+                    <p className="text-gray-600">{task.description}</p>
+                  </div>
 
-        <div className="space-x-2">
-          <button
-            onClick={() => handleEdit(task)}
-            className="btn btn-sm btn-warning"
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => handleDelete(task.id)}
-            className="btn btn-sm btn-error"
-          >
-            Delete
-          </button>
-        </div>
-      </li>
-    ))
-  ) : (
-    <p className="text-gray-500">No tasks yet</p>
-  )}
-</ul>
+                  <div className="space-x-2">
+                    <button
+                      onClick={() => handleEdit(task)}
+                      className="btn btn-sm btn-warning"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDelete(task.id)}
+                      className="btn btn-sm btn-error"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p className="text-gray-500">No tasks yet</p>
+            )}
+      </ul>
+            <button
+              onClick={handleLogout}
+              className="btn btn-error w-full mt-8 "
+            >
+              Exit
+            </button>
+      </div>
     </div>
   );
 };
